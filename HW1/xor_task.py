48ecf016d15d6324c3fcd8af0c53d2c6f4c3f88f
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2024-04-24 17:53:28 -0400

    final submissino

--- HW1/xor_task.py
+++ HW1/xor_task.py
@@ -24,7 +24,7 @@ net = create_net(in_features, hidden_units, non_linearity, out_size)
 
 # specify the training opts
 train_opts = {
-    "num_epochs": 15,
+    "num_epochs": 20, # used 20 because it took around 18 epochs to get to 100% accuracy
     "lr": 0.5,
     "momentum": 0.9,
     "batch_size": 4,
@@ -36,10 +36,3 @@ train_opts = {
 # train and save the model with base options
 train(net, xor_dataset, train_opts)
 save(net, 'xor_solution.pt')
-
-
-# train and save the model for 30 epochs because
-# accuracy seems to reach 100% after 18 epochs
-train_opts["num_epochs"] = 20
-train(net, xor_dataset, train_opts)
-save(net, 'xor_solution_30epochs.pt')
